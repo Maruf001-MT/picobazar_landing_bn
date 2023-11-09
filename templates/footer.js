19c8d85a92940d9b5807040bac3c7c3ec9421db6
Maruf Islam <jisanmaruf562@gmail.com>
2023-11-09 16:29:14 +0600

    maruf

---
+++ templates/footer.js
@@ -0,0 +1,136 @@
+class FooterTemplate extends HTMLElement {
+    connectedCallback() {
+      this.innerHTML = `
+
+      <div class="footer">
+        <div class="container">
+            <div class="footer-content">
+                <div id="w-node-_74ab3314-1e2b-a768-0bb3-b25894c1a502-0a144c54" class="content">
+                    <div class="fade-in-move-on-scroll">
+                        <div class="footer-brand-content"><a href="#" class="footer-logo-link"><img src="https://drive.prozukty.com/share/2P7Rv7AJhS4X3fGr" alt="SaaS Webflow Template - Frankfurt - Created by Wedoflow.com and Azwedo.com" class="footer-image"/></a>
+                            <div
+                                class="footer-description">Crafted with love️ by <a href="https://azwedo.com?ref=frankfurt" target="_blank" class="link">Azwedo</a> &amp; <a href="https://wedoflow.com?ref=frankfurt" target="_blank" class="link">Wedoflow<br/>‍</a>Powered by <a href="https://webflow.com?ref=wedoflow.com"
+                                    target="_blank" class="link">Webflow</a></div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-grid-content">
+                <div class="footer-heading">Product</div>
+                <div class="fade-in-move-on-scroll">
+                    <a href="#" class="footer-link w-inline-block">
+                        <div>Platform</div>
+                        <div class="link-block-underline"></div>
+                    </a>
+                </div>
+                <div class="fade-in-move-on-scroll">
+                    <a href="/features" class="footer-link w-inline-block">
+                        <div>Features</div>
+                        <div class="link-block-underline"></div>
+                    </a>
+                </div>
+                <div class="fade-in-move-on-scroll">
+                    <a href="/contact" class="footer-link w-inline-block">
+                        <div>Contact</div>
+                        <div class="link-block-underline"></div>
+                    </a>
+                </div>
+                <div class="fade-in-move-on-scroll">
+                    <a href="/about" class="footer-link w-inline-block">
+                        <div>About</div>
+                        <div class="link-block-underline"></div>
+                    </a>
+                </div>
+                <div class="fade-in-move-on-scroll">
+                    <a href="/shop" class="footer-link w-inline-block">
+                        <div>Our Shop</div>
+                        <div class="link-block-underline"></div>
+                    </a>
+                </div>
+            </div>
+            <div class="footer-grid-content">
+                <div class="footer-heading">Legal</div>
+                <div class="fade-in-move-on-scroll">
+                    <a href="/privacy-policy" class="footer-link w-inline-block">
+                        <div>Privacy</div>
+                        <div class="link-block-underline"></div>
+                    </a>
+                </div>
+                <div class="fade-in-move-on-scroll">
+                    <a href="#" class="footer-link w-inline-block">
+                        <div>Password</div>
+                        <div class="link-block-underline"></div>
+                    </a>
+                </div>
+                <div class="fade-in-move-on-scroll">
+                    <a href="/blog" class="footer-link w-inline-block">
+                        <div>Blog</div>
+                        <div class="link-block-underline"></div>
+                    </a>
+                </div>
+            </div>
+            <div class="footer-grid-content">
+                <div class="footer-heading">Template</div>
+                <div class="fade-in-move-on-scroll">
+                    <a href="/template/style-guide" class="footer-link w-inline-block">
+                        <div>Style Guide</div>
+                        <div class="link-block-underline"></div>
+                    </a>
+                </div>
+                <div class="fade-in-move-on-scroll">
+                    <a href="/template/licensing" class="footer-link w-inline-block">
+                        <div>Licencing</div>
+                        <div class="link-block-underline"></div>
+                    </a>
+                </div>
+                <div class="fade-in-move-on-scroll">
+                    <a href="/template/change-log" class="footer-link w-inline-block">
+                        <div>Change Log</div>
+                        <div class="link-block-underline"></div>
+                    </a>
+                </div>
+                <div class="fade-in-move-on-scroll">
+                    <a href="/template/instructions" class="footer-link w-inline-block">
+                        <div>Instructions</div>
+                        <div class="link-block-underline"></div>
+                    </a>
+                </div>
+            </div>
+            <div class="footer-grid-content">
+                <div class="footer-heading">Social</div>
+                <div class="fade-in-move-on-scroll">
+                    <a href="http://facebook.com" target="_blank" class="footer-link w-inline-block">
+                        <div>Facebook</div>
+                        <div class="link-block-underline"></div>
+                    </a>
+                </div>
+                <div class="fade-in-move-on-scroll">
+                    <a href="http://instagram.com" target="_blank" class="footer-link w-inline-block">
+                        <div>Instagram</div>
+                        <div class="link-block-underline"></div>
+                    </a>
+                </div>
+                <div class="fade-in-move-on-scroll">
+                    <a href="http://Linekdin.com" target="_blank" class="footer-link w-inline-block">
+                        <div>Linked In</div>
+                        <div class="link-block-underline"></div>
+                    </a>
+                </div>
+                <div class="fade-in-move-on-scroll">
+                    <a href="http://twitter.com" target="_blank" class="footer-link w-inline-block">
+                        <div>Twitter</div>
+                        <div class="link-block-underline"></div>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div data-w-id="eb050d0c-cd20-6fbc-5d31-0eaf0a144c87" class="made-by-section">
+            <div class="copyright-text">© Copyright 2023 Frankfurt.  All rights reserved.</div>
+        </div>
+    </div>
+    </div>
+
+      `;
+    }
+  }
+
+  customElements.define('footer-template', FooterTemplate);
