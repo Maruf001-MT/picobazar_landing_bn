3dee8f97a6ef415778020796cb3d95de65020f39
Maruf Islam <jisanmaruf562@gmail.com>
2023-11-10 18:40:11 +0600

    asdasd

--- career/apply/mail.php
+++ career/apply/mail.php
@@ -14,38 +14,32 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $linkedin = $_POST["Linkedin"];
     $github = $_POST["Github"];
 
+
     // Check if the 'agree_terms' checkbox is selected
-    if (isset($_POST['Agree']) && $_POST['Agree'] == 'on') {
-        // Process file upload (resume)
-        $uploadDir = "uploads/"; // Specify the directory where you want to store uploaded files
-        $resumePath = $uploadDir . basename($_FILES["Resume"]["name"]);
+    if (isset($_POST['agree_terms']) && $_POST['agree_terms'] == 'on') {
 
-        if (move_uploaded_file($_FILES["Resume"]["tmp_name"], $resumePath)) {
-            // Build the email message
-            $message = "Full Name: $name\n";
-            $message .= "Email: $email\n";
-            $message .= "Phone Number: $phone\n";
-            $message .= "Age: $age\n";
-            $message .= "Address: $address\n\n";
-            $message .= "Experiences: $experience\n";
-            $message .= "Skills: $skills\n\n";
-            $message .= "LinkedIn Profile: $linkedin\n";
-            $message .= "Github Profile: $github\n";
+        // Build the email message
+        $message = "Full Name: $name\n";
+        $message .= "Email: $email\n";
+        $message .= "Phone Number: $phone\n";
+        $message .= "Age: $age\n";
+        $message .= "Address: $address\n\n";
+        $message .= "Experiences: $experience\n";
+        $message .= "Skills: $skills\n\n";
+        $message .= "LinkedIn Profile: $linkedin\n";
+        $message .= "Github Profile: $github\n";
 
-            // Set additional email headers
-            $headers = "From: $email" . "\r\n" .
-            "Reply-To: $email" . "\r\n" .
-            "X-Mailer: PHP/" . phpversion();
+        // Set additional email headers
+        $headers = "From: $email" . "\r\n" .
+        "Reply-To: $email" . "\r\n" .
+        "X-Mailer: PHP/" . phpversion();
 
-            // Send the email
-            if (mail($to, $subject, $message, $headers)) {
-                header("Location: thank.html");
-                exit();
-            } else {
-                echo "Sorry, there was an error sending your application. Please try again later.";
-            }
+        // Send the email
+        if (mail($to, $subject, $message, $headers)) {
+            header("Location: thank.html");
+            exit();
         } else {
-            echo "Sorry, there was an error uploading your resume.";
+            echo "Sorry, there was an error sending your application. Please try again later.";
         }
     } else {
         echo "Please agree to the Terms and Conditions.";
